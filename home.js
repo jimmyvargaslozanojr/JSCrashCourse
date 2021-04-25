4309522583111a8f78f23ba8cb24e2f9b6d502e2
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-04-25 17:43:31 +0800

    while and for loop

--- home.js
+++ home.js
@@ -1,28 +1,58 @@
-console.log('hello');
+// console.log('hello');
 // alert('yooo');
 
 //Variables
 
-var b = 'smoothie';
-console.log(b);
+// var b = 'smoothie';
+// console.log(b);
 
-var someNumber = 45;
-console.log(someNumber);
-
-
+// var someNumber = 45;
+// console.log(someNumber);
 
 // var age = prompt('What is your age?');
 // console.log(age);
 
 // document.getElementById('sometext').innerHTML = age;
 
-var num1 = 10;
-num1 = num1 + 1;
-console.log(num1);
+// var num1 = 10;
+// num1 = num1 + 1;
+// console.log(num1);
 
 // Divide, Multiply *, Remainder %
 
-console.log(num1 % 6);
+// console.log(num1 % 6);
 
-num1 += 10;
-console.log(num1);
+// num1 += 10;
+// console.log(num1);
+
+// Fuction
+
+// function fun(){
+//     console.log('this is a function');
+// }
+// fun();
+
+// function greeting(youName){
+//     var result = 'Hello'+ ' ' + youName;
+//     console.log(result);
+// }
+// var name = prompt('What is yout name');
+// greeting(name);
+
+// greeting();
+
+// function sumNumbers(num1,num2){
+//     var result = num1 + num2;
+//     console.log(result);
+// }
+// sumNumbers(32,12);
+
+// var num = 0;
+// while (num < 100){
+//     num += 1;
+//     console.log(num);
+// }
+
+// for(let num = 0; num <= 100 ; num++){
+//     console.log(num);
+// }
