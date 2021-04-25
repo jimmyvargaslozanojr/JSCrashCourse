677baac90c6239ce3da2b8dc8689428e39260e2b
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-04-25 16:59:19 +0800

    calculation

--- home.js
+++ home.js
@@ -1,2 +1,17 @@
 console.log('hello');
-alert('yooo');
+// alert('yooo');
+
+//Variables
+
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+
+
+var age = prompt('What is your age?');
+console.log(age);
+
+document.getElementById('sometext').innerHTML = age;
