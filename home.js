f22ae13c53841a82e49ad9c9d01f2bad5eefb407
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-04-25 17:33:13 +0800

    functions

--- home.js
+++ home.js
@@ -11,7 +11,18 @@ console.log(someNumber);
 
 
 
-var age = prompt('What is your age?');
-console.log(age);
+// var age = prompt('What is your age?');
+// console.log(age);
 
-document.getElementById('sometext').innerHTML = age;
+// document.getElementById('sometext').innerHTML = age;
+
+var num1 = 10;
+num1 = num1 + 1;
+console.log(num1);
+
+// Divide, Multiply *, Remainder %
+
+console.log(num1 % 6);
+
+num1 += 10;
+console.log(num1);
